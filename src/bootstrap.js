a22f25b90d60914dd859f8d69b918a13a9aac472
Luis_Cintron <l.cintron9966@outlook.com>
2021-02-03 20:21:29 -0500

    built out layout grid and header and navbar components

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -5,9 +5,10 @@ import { createStore, applyMiddleware } from "redux";
 import { BrowserRouter } from "react-router-dom";
 import reducers from "./reducers";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-import "./style/main.scss";
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+import './style/main.scss';
+
 import Layout from "./components/layout";
 
 function main() {
