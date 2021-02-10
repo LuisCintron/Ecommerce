d865ffab2558b3b8af4589e8c10d3c8d8de60d5b
Luis_Cintron <l.cintron9966@outlook.com>
2021-02-09 21:38:04 -0500

    created a page title component and style it

---
+++ src/components/pageTitle.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class pageTitle extends Component {
+  render() {
+    const { className, title } = this.props;
+    return (
+      <div className={`${className} page-title`}>
+        {title}
+      </div>
+    )
+  }
+}
+export default pageTitle;
